87a0d2db85c039d9ec50d6336f287fa0256c33d2
Pranav <pranavtbhat@gmail.com>
2017-03-03 20:11:58 -0800

    Encoding strings to integers

--- parse.py
+++ parse.py
@@ -3,6 +3,7 @@ from os import listdir
 from os.path import isfile, join
 import networkx as nx
 from tqdm import tqdm
+import sys
 
 # First get a list of valid ego users
 GPLUS_DIR = "dataset/gplus/"
@@ -15,8 +16,34 @@ ego_users = np.unique(
 
 G = nx.DiGraph()
 
+
+encode_kv_count = 0
+def encode_kv(d, t):
+    global encode_kv_count
+    if t in d:
+        return d[t]
+    else:
+        d[t] = encode_kv_count
+        encode_kv_count += 1
+        return encode_kv_count - 1
+
+
+encode_k_count = 0
+def encode_k(d, k):
+    global encode_k_count
+    if k in d:
+        return d[k]
+    else:
+        d[k] = encode_k_count
+        encode_k_count += 1
+        return encode_k_count - 1
+
+
 print "Extracting vertex data"
-for ego_user in tqdm(ego_users):
+for ego_user in tqdm(ego_users[0:1]):
+    # Encodings
+    k_dict ={}
+    kv_dict = {}
 
     # Parse the featnames
     propnames = []
@@ -26,6 +53,7 @@ for ego_user in tqdm(ego_users):
         for line in fnf:
             vid, prop = line.rstrip().split(' ', 1)
             propname, value = prop.split(':', 1)
+
             propnames.append(propname)
             propvalues.append(value)
 
@@ -37,7 +65,9 @@ for ego_user in tqdm(ego_users):
         arr = np.fromfile(efeat, sep=' ', dtype=int)
         indexes, = np.nonzero(arr)
         for i in indexes:
-            G[int(ego_user)][propnames[i]] = propvalues[i]
+            prop = encode_k(k_dict, propnames[i])
+            val = encode_kv(kv_dict, (propnames[i], propvalues[i]))
+            G[int(ego_user)][prop] = val
 
     # Fetch properties for all other nodes
     with open(join(GPLUS_DIR, ego_user + '.feat'), 'r') as feat:
@@ -55,10 +85,14 @@ for ego_user in tqdm(ego_users):
 
             # Add properties to graph
             for i in indexes:
-                G[int(vertex)][propnames[i]] = propvalues[i]
+                prop = encode_k(k_dict, propnames[i])
+                val = encode_kv(kv_dict, (propnames[i], propvalues[i]))
+                G[int(vertex)][prop] = val
 
+sys.exit(0)
 print "Extracting edge data"
 with open(join('dataset', 'gplus_combined.txt')) as edata:
     for line in tqdm(edata):
         u, v = line.rstrip().split(' ')
         G.add_edge(int(u), int(v))
+
