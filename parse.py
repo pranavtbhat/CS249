ac2b45e5ec9abaf6f65e61d208235c8a54f28831
Pranav <pranavtbhat@gmail.com>
2017-03-07 15:26:44 -0800

    new stuff

--- parse.py
+++ parse.py
@@ -38,9 +38,20 @@ def encode_k(d, k):
         encode_k_count += 1
         return encode_k_count - 1
 
+vertex_count = 0
+def encode_vertex(d, v):
+    global vertex_count
+    if v in d:
+        return d[v]
+    else:
+        d[v] = vertex_count
+        vertex_count += 1
+        return vertex_count - 1
+
 # Encodings
-k_dict ={}
+k_dict = {}
 kv_dict = {}
+v_dict = {}
 
 # Parse the featnames
 propnames = []
@@ -58,16 +69,17 @@ for ego_user in tqdm(ego_users[0:3]):
             propvalues.append(value)
 
     # Insert the ego node itself
-    G.add_node(int(ego_user))
+    G.add_node(encode_vertex(v_dict, int(ego_user)))
 
     # Fetch the ego nodes properties
     with open(join(GPLUS_DIR, ego_user + '.egofeat'), 'r') as efeat:
         arr = np.fromfile(efeat, sep=' ', dtype=int)
         indexes, = np.nonzero(arr)
         for i in indexes:
+            v = encode_vertex(v_dict, int(ego_user))
             prop = encode_k(k_dict, propnames[i])
             val = encode_kv(kv_dict, (propnames[i], propvalues[i]))
-            G[int(ego_user)][prop] = val
+            G.node[v][prop] = val
 
     # Fetch properties for all other nodes
     with open(join(GPLUS_DIR, ego_user + '.feat'), 'r') as feat:
@@ -81,17 +93,21 @@ for ego_user in tqdm(ego_users[0:3]):
             indexes, = np.nonzero(arr)
 
             # Add vertex to graph
-            G.add_node(int(vertex))
+            v = encode_vertex(v_dict, int(vertex))
+            G.add_node(v)
 
             # Add properties to graph
             for i in indexes:
                 prop = encode_k(k_dict, propnames[i])
                 val = encode_kv(kv_dict, (propnames[i], propvalues[i]))
-                G[int(vertex)][prop] = val
+                G.node[v][prop] = val
 
 print "Extracting edge data"
 with open(join('dataset', 'gplus_combined.txt')) as edata:
     for line in tqdm(edata):
         u, v = line.rstrip().split(' ')
-        G.add_edge(int(u), int(v))
+        u = v_dict[int(u)]
+        v = v_dict[int(v)]
+        print u, v
+        G.add_edge(u, v)
 
